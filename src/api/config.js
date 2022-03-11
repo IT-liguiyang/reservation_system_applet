a1f6609e6957b7bf520df02125386a645172a365
IT-liguiyang <1781605904@qq.com>
2022-03-11 13:08:36 +0800

    fix: 修改发送请求函数

---
+++ src/api/config.js
@@ -0,0 +1,8 @@
+// 配置服务器相关信息
+export default {
+  host: 'http://localhost:5000',
+  basicImgUrl: 'http://localhost:5000/upload/',
+
+  // host: 'http://4966w0186v.qicp.vip', // 设置内网穿透
+  // basicImgUrl: 'http://4966w0186v.qicp.vip/upload//',
+};
--- src/api/request.js
+++ src/api/request.js
@@ -10,34 +10,32 @@
  */
 
 import Taro from '@tarojs/taro';
+import config from './config';
 
-export default function ajax(url, data={}, type='GET') {
+export default (url, data={}, type='GET') => {
 
-  return new Promise((resolve) => {
-    let promise;
-    // 1. 执行异步ajax请求
-    if(type==='GET') { // 发GET请求
-      promise = Taro.request({
-        url,
-        method: 'GET',
-        data
-      });
-    } else { // 发POST请求
-      promise = Taro.request({
-        url,
-        method: 'POST',
-        // data
-      });
-    }
-    // 2. 如果成功了, 调用resolve(value)
-    promise.then(response => {
-      resolve(response.data);
-    // 3. 如果失败了, 不调用reject(reason), 而是提示异常信息
-    }).catch(error => {
-      // reject(error)
-      console.log('请求出错了: ' + error);
+  return new Promise((resolve, reject) => {
+    // 1. 执行异步请求
+    Taro.request({
+      url: config.host + url,
+      data,
+      method: type,
+      // 2. 如果成功了, 调用resolve(value)
+      success: (res) => {
+        // if(data.isLogin){// 登录请求
+        //   // 将用户的cookie存入至本地
+        //   wx.setStorage({
+        //     key: 'cookies',
+        //     data: res.cookies
+        //   })
+        // }
+        resolve(res.data); // resolve修改promise的状态为成功状态resolved
+      },
+      // 3. 如果失败了, 不调用reject(reason), 而是提示异常信息
+      fail: (err) => {
+        // console.log('请求失败: ', err);
+        reject(err); // reject修改promise的状态为失败状态 rejected
+      }
     });
   });
-
-}
-
+};
