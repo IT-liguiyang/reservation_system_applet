2d2df304fdba7a55421e67a661b2f8e313085a7f
IT-liguiyang <1781605904@qq.com>
2022-03-11 13:06:58 +0800

    fix: 配置云函数

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import { Component } from 'react';
+import Taro from '@tarojs/taro';
 
 import './app.less';
+import './static/iconfont/iconfont.css'; // 引入全局的icon样式
 
 class App extends Component {
 
-  componentDidMount () {}
+  componentDidMount () {
+    Taro.cloud.init({
+      env: 'reservation-system-7diwnb2d9a3a5'
+    });
+  }
 
   componentDidShow () {}
 
