4dd51169c67119cc9b8052b76962b175ebab0b3d
IT-liguiyang <1781605904@qq.com>
2022-04-19 13:18:28 +0800

    fix: 调整接口请求函数

--- src/pages/school/service.jsx
+++ src/pages/school/service.jsx
@@ -7,4 +7,4 @@ export const reqAnnouncementByPublisher = (publisher) => Request('/manage/announ
 export const reqBookingInfoBySchooId = (school_id) => Request('/manage/booking_info/search_by_school_id', {school_id});
 
 // 通过学校 id 查询预约信息
-export const reqReservationInfoBySchoolId = (school_id) => Request('/manage/reservation_info/list_by_school_id', {school_id});
+export const reqReservationInfoBySchoolId = (school_id) => Request('/wechat/reservation_info/list_by_school_id', {school_id});
